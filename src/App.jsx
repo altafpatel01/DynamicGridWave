a14913cca59876fc0a1605bda6cd691ca5ad3357
Altaf <altafpatel8485@gmail.com>
2025-01-21 18:27:40 +0530

    Initial commit for the project setup like install reactjs using vite and also install twailwind and config it

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
