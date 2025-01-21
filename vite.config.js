48d9e0f138c14523db3cc976465140f4c29ce72b
Altaf <altafpatel8485@gmail.com>
2025-01-21 21:11:09 +0530

    Update Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DynamicGridWave/', // Adjust this to match your GitHub repo name
   plugins: [react()],
-})
+});
